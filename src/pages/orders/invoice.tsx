064790e5ebb4498853ac82497c934da4852471d4
unknown <christianramirez1996@hotmail.com>
2023-06-08 17:12:16 -0400

    get invoice data by id

--- src/pages/orders/invoice.tsx
+++ src/pages/orders/invoice.tsx
@@ -1,4 +1,77 @@
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import type { AppState } from "../../store";
+
+const findBillingById = (state: AppState, id: string) => {
+  return state.billings.find((billing: any) => billing.id === id) || null;
+};
+
 const InvoiceDetail = () => {
+  const router = useRouter();
+  const { billingId } = router.query;
+
+  const baseBillings = useSelector((state: any) => state.billings);
+
+  const [billing, setBillingState] = useState({
+    id: "",
+    billingNumber: "",
+    issueDate: "",
+    bussiness: {
+      rut: "",
+      name: "",
+      commercialTurn: "",
+      address: "",
+      email: "",
+      image: "",
+      city: "",
+    },
+    customer: {
+      id: "",
+      rut: "",
+      isLegalEntity: false,
+      fullName: "",
+      commercialTurn: "",
+      address: "",
+      city: "",
+      phone: "",
+      email: "",
+    },
+    workOrder: {
+      id: "",
+      receiptDate: "",
+      deliveryDate: "",
+      licensePlate: "",
+      responsibleMechanic: "",
+      responsibleMechanicId: "",
+      cost: 0,
+    },
+    billing: "",
+    netAmount: 0,
+    fines: 0,
+    otherTaxes: 0,
+    subtotal: 0,
+    iva: 0,
+    totalAmount: 0,
+  });
+
+  useEffect(() => {
+    console.log("CreateVehicle useEffect");
+
+    if (billingId?.length) {
+      const foundBilling = findBillingById(
+        baseBillings,
+        billingId as string
+      );
+      console.log("Found billing: ", foundBilling);
+
+      if (foundBilling) {
+        setBillingState(foundBilling);
+        console.log("Setting billing state: ", foundBilling);
+      }
+    }
+  }, [billingId, baseBillings]);
+
   return (
     <div className="flex items-center justify-center py-4 md:py-2">
       <div className="bg-white rounded-lg shadow-lg px-6 py-8 max-w-md mx-auto mt-16">
@@ -9,16 +82,16 @@ const InvoiceDetail = () => {
         <div className="flex justify-between mb-6">
           <h1 className="text-lg font-bold">Invoice</h1>
           <div className="text-gray-700">
-            <div>Fecha: 01/05/2023</div>
-            <div>Número Factura: #12345</div>
+            <div>Fecha: {billing.issueDate}</div>
+            <div>Número Factura: #{billing.billingNumber}</div>
           </div>
         </div>
         <div className="mb-8">
           <h2 className="text-lg font-bold mb-4">Facturar A:</h2>
-          <div className="text-gray-700 mb-2">John Doe</div>
-          <div className="text-gray-700 mb-2">123 Main St.</div>
-          <div className="text-gray-700 mb-2">Anytown, USA 12345</div>
-          <div className="text-gray-700">johndoe@example.com</div>
+          <div className="text-gray-700 mb-2">{billing.customer.isLegalEntity ? billing.customer.commercialTurn : billing.customer.fullName}</div>
+          <div className="text-gray-700 mb-2">{billing.customer.address}</div>
+          <div className="text-gray-700 mb-2">{billing.customer.city}</div>
+          <div className="text-gray-700">{billing.customer.email}</div>
         </div>
         <table className="w-full mb-8">
           <thead>
